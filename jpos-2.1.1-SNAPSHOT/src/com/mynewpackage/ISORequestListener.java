83d2302afd72c6d5f745a194a444344b9017d81c
19-HRDYTP <63578751+19-HRDYTP@users.noreply.github.com>
2024-08-27 13:56:07 +0530

    Update ISORequestListener.java

--- jpos-2.1.1-SNAPSHOT/src/com/mynewpackage/ISORequestListener.java
+++ jpos-2.1.1-SNAPSHOT/src/com/mynewpackage/ISORequestListener.java
@@ -4,13 +4,7 @@ import org.jpos.iso.ISOMsg;
 import org.jpos.iso.ISOSource;
 
 public interface ISORequestListener {
-    /**
-     * Processes an incoming ISO 8583 message.
-     *
-     * @param source the source of the ISO message
-     * @param msg the ISO message
-     * @return true if the message was processed successfully, false otherwise
-     */
+    
     boolean process(ISOSource source, ISOMsg msg);
 }
 
