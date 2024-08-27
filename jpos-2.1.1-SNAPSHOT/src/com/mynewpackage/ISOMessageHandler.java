22b5758356546efa43b172acc0a54845113cdf26
19-HRDYTP <63578751+19-HRDYTP@users.noreply.github.com>
2024-08-27 13:57:17 +0530

    Update ISOMessageHandler.java

--- jpos-2.1.1-SNAPSHOT/src/com/mynewpackage/ISOMessageHandler.java
+++ jpos-2.1.1-SNAPSHOT/src/com/mynewpackage/ISOMessageHandler.java
@@ -15,13 +15,9 @@ public class ISOMessageHandler implements ISORequestListener, org.jpos.iso.ISORe
     public boolean process(ISOSource source, ISOMsg m) {
         try {
 
-            // Set the response MTI based on the received message
+            
             m.setResponseMTI();
-
-            // Set a response code (e.g., "00" for success)
             m.set(39, "00");
-
-            // Send the response back to the source
             source.send(m);
 
         } catch (ISOException | IOException e) {
@@ -32,22 +28,22 @@ public class ISOMessageHandler implements ISORequestListener, org.jpos.iso.ISORe
 
 
     public static void main(String[] args) throws Exception {
-        // Set up logging
+       
         Logger logger = new Logger();
         logger.addListener(new SimpleLogListener(System.out));
 
-        // Set up the channel with XMLPackager
+       
         ServerChannel channel = new XMLChannel(new XMLPackager());
         ((LogSource) channel).setLogger(logger, "channel");
 
-        // Set up the server
+       
         ISOServer server = new ISOServer(8000, channel, null);
         server.setLogger(logger, "server");
 
-        // Add the request listener
+       
         server.addISORequestListener(new ISOMessageHandler());
 
-        // Start the server in a new thread
+        
         new Thread(server).start();
 
         System.out.println("ISOMessageServer is running...");
